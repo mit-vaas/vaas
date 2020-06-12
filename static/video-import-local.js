d93b4e2f695508fcbbb109a68e53fb80209ae3bd
Favyen Bastani <fbastani@perennate.com>
2020-06-12 16:22:13 -0400

    Reduce indent of vue.js templates

--- static/video-import-local.js
+++ static/video-import-local.js
@@ -23,35 +23,35 @@ Vue.component('video-import-local', {
 		},
 	},
 	template: `
-		<span>
-			<button type="button" class="btn btn-primary" v-on:click=click>Import from Local</button>
-			<div class="modal" tabindex="-1" role="dialog" id="v-local-modal">
-				<div class="modal-dialog" role="document">
-					<div class="modal-content">
-						<div class="modal-body">
-							<form v-on:submit.prevent="submit">
-								<div class="form-group row">
-									<label class="col-sm-2 col-form-label">Name</label>
-									<div class="col-sm-10">
-										<input class="form-control" type="text" v-model="name" />
-									</div>
-								</div>
-								<div class="form-group row">
-									<label class="col-sm-2 col-form-label">Path</label>
-									<div class="col-sm-10">
-										<input class="form-control" type="text" v-model="path" />
-									</div>
-								</div>
-								<div class="form-group row">
-									<div class="col-sm-10">
-										<button type="submit" class="btn btn-primary">Import</button>
-									</div>
-								</div>
-							</form>
+<span>
+	<button type="button" class="btn btn-primary" v-on:click=click>Import from Local</button>
+	<div class="modal" tabindex="-1" role="dialog" id="v-local-modal">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-body">
+					<form v-on:submit.prevent="submit">
+						<div class="form-group row">
+							<label class="col-sm-2 col-form-label">Name</label>
+							<div class="col-sm-10">
+								<input class="form-control" type="text" v-model="name" />
+							</div>
 						</div>
-					</div>
+						<div class="form-group row">
+							<label class="col-sm-2 col-form-label">Path</label>
+							<div class="col-sm-10">
+								<input class="form-control" type="text" v-model="path" />
+							</div>
+						</div>
+						<div class="form-group row">
+							<div class="col-sm-10">
+								<button type="submit" class="btn btn-primary">Import</button>
+							</div>
+						</div>
+					</form>
 				</div>
 			</div>
-		</span>
+		</div>
+	</div>
+</span>
 	`,
 });
--- static/video.js
+++ static/video.js
@@ -19,30 +19,30 @@ Vue.component('video-tab', {
 		},
 	},
 	template: `
-		<div>
-			<div class="my-1">
-				<video-import-local v-on:imported="fetchVideos"></video-import-local>
-				<video-import-youtube v-on:imported="fetchVideos"></video-import-youtube>
-			</div>
-			<table class="table">
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Progress</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr v-for="video in videos">
-						<td>{{ video.Name }}</td>
-						<template v-if="video.Percent == 100">
-							<td>Ready</td>
-						</template>
-						<template v-else>
-							<td>{{ video.Percent }}%</td>
-						</template>
-					</tr>
-				</tbody>
-			</table>
-		</div>
+<div>
+	<div class="my-1">
+		<video-import-local v-on:imported="fetchVideos"></video-import-local>
+		<video-import-youtube v-on:imported="fetchVideos"></video-import-youtube>
+	</div>
+	<table class="table">
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Progress</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr v-for="video in videos">
+				<td>{{ video.Name }}</td>
+				<template v-if="video.Percent == 100">
+					<td>Ready</td>
+				</template>
+				<template v-else>
+					<td>{{ video.Percent }}%</td>
+				</template>
+			</tr>
+		</tbody>
+	</table>
+</div>
 	`,
 });
