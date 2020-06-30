a6478d961924064a9841b837991550db42755d4c
Favyen Bastani <fbastani@perennate.com>
2020-06-30 14:18:40 -0400

    job-cmd details: scroll to bottom and refresh

--- static/job-cmd.js
+++ static/job-cmd.js
@@ -6,13 +6,31 @@ Vue.component('job-cmd', {
 	},
 	props: ['job'],
 	created: function() {
-		$.get('/jobs/detail', {job_id: this.job.ID}, (lines) => {
-			this.lines = lines.join("\n");
-		}, 'json');
+		this.update(true);
+		setInterval(this.update, 1000);
+	},
+	destroyed: function() {
+		clearInterval(this.interval);
+	},
+	methods: {
+		update: function(first) {
+			$.get('/jobs/detail', {job_id: this.job.ID}, (lines) => {
+				this.lines = lines;
+				if(first || window.innerHeight + window.scrollY >= document.body.scrollHeight) {
+					Vue.nextTick(() => {
+						window.scrollTo(0, document.body.scrollHeight);
+					});
+				}
+			}, 'json');
+		},
 	},
 	template: `
 <div>
-	<pre>{{ lines }}</pre>
+	<div class="plaintext-div">
+		<template v-for="line in lines">
+			{{ line }}<br />
+		</template>
+	</div>
 </div>
 	`,
 });
