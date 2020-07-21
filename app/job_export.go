596544ce38b510ed6c569856b2fa1dd0919dbb8c
Favyen Bastani <fbastani@perennate.com>
2020-07-21 15:33:13 -0400

    add freq option for export job to specify freq of exported files

--- app/job_export.go
+++ app/job_export.go
@@ -41,6 +41,9 @@ type ExportOptions struct {
 
 	// Custom job name
 	Name string
+
+	// export all items at this freq
+	Freq int
 }
 
 type Exporter struct {
@@ -73,9 +76,9 @@ func (e *Exporter) Run(statusFunc func(string)) error {
 		if ref.Item == nil {
 			return fmt.Errorf("error exporting video: video must be from item not encoded")
 		}
-		rd := vaas.ReadVideo(*ref.Item, slice, vaas.ReadVideoOptions{})
-		defer rd.Close()
 		if slice.Length() == 1 {
+			rd := vaas.ReadVideo(*ref.Item, slice, vaas.ReadVideoOptions{})
+			defer rd.Close()
 			im, err := rd.Read()
 			if err != nil {
 				return err
@@ -83,7 +86,27 @@ func (e *Exporter) Run(statusFunc func(string)) error {
 			if err := ioutil.WriteFile(prefix + ".jpg", im.AsJPG(), 0644); err != nil {
 				return err
 			}
+		} else if ref.Item.Slice.Equals(slice) && (e.opts.Freq == 0 || e.opts.Freq == ref.Item.Freq) && ref.Item.Format == "mp4" {
+			// copy the file contents directly when possible
+			srcFile, err := os.Open(ref.Item.Fname(0))
+			if err != nil {
+				return err
+			}
+			defer srcFile.Close()
+			dstFile, err := os.Create(prefix + ".mp4")
+			if err != nil {
+				return err
+			}
+			if _, err := io.Copy(dstFile, srcFile); err != nil {
+				return err
+			}
 		} else {
+			var videoOpts vaas.ReadVideoOptions
+			if e.opts.Freq != 0 {
+				videoOpts.Sample = e.opts.Freq / ref.Item.Freq
+			}
+			rd := vaas.ReadVideo(*ref.Item, slice, videoOpts)
+			defer rd.Close()
 			file, err := os.Create(prefix + ".mp4")
 			if err != nil {
 				return err
@@ -98,6 +121,7 @@ func (e *Exporter) Run(statusFunc func(string)) error {
 
 	exportOther := func(slice vaas.Slice, ref ConcreteRef, prefix string) error {
 		var data vaas.Data
+		var freq int
 		if ref.Item != nil {
 			buf := ref.Item.Load(slice)
 			var err error
@@ -105,8 +129,13 @@ func (e *Exporter) Run(statusFunc func(string)) error {
 			if err != nil {
 				return fmt.Errorf("error reading buffer: %v", err)
 			}
+			freq = ref.Item.Freq
 		} else {
 			data = ref.Data.Slice(slice.Start - ref.Slice.Start, slice.End - ref.Slice.End)
+			freq = 1
+		}
+		if e.opts.Freq != 0 {
+			data = vaas.AdjustDataFreq(data, slice.Length(), freq, e.opts.Freq)
 		}
 		fname := prefix + ".json"
 		if err := ioutil.WriteFile(fname, data.Encode(), 0644); err != nil {
@@ -115,9 +144,7 @@ func (e *Exporter) Run(statusFunc func(string)) error {
 		return nil
 	}
 
-	fmt.Println("start", e.refs)
 	err := EnumerateDataRefs(e.refs, func(slice vaas.Slice, refs []ConcreteRef) error {
-		fmt.Println("one", refs)
 		prefix := fmt.Sprintf("%s/%d_%d_%d", e.opts.Path, slice.Segment.ID, slice.Start, slice.End)
 		for i, ref := range refs {
 			var err error
