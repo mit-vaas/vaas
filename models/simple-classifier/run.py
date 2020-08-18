e5b555b86ffe7eaaba48634f9108da90af9c35e9
Favyen Bastani <fbastani@perennate.com>
2020-08-18 15:01:36 -0400

    fix bugs in simple classifier

--- models/simple-classifier/run.py
+++ models/simple-classifier/run.py
@@ -8,7 +8,7 @@ import struct
 import sys
 
 sys.path.append('.')
-import skyhook_pylib
+import skyhook_pylib as lib
 
 model_path = sys.argv[1]
 num_classes = int(sys.argv[2])
@@ -19,7 +19,7 @@ m = model.get_model(num_classes, width, height)
 m.load_weights(model_path)
 
 @lib.per_frame_decorate
-def f(im)
+def f(im):
 	if im.shape[0] != height or im.shape[1] != width:
 		im = skimage.transform.resize(im, [height, width], preserve_range=True).astype('uint8')
 	pred = m.predict(numpy.expand_dims(im, axis=0))[0]
